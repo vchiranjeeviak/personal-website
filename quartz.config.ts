944843d8b97b3a283b8761baf20b3bc2815bf509
vchiranjeeviak <vchiranjeeviak.tirunagari@gmail.com>
2025-02-05 01:57:58 +0530

    chore: secondary color change

--- quartz.config.ts
+++ quartz.config.ts
@@ -47,7 +47,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
+          secondary: "#ce7e00",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
